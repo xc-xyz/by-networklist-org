6ae780335e9c5fdcbc29488f1afe620d6c858781
xc-link <cai_liyong@qq.com>
2024-10-01 03:03:45 +0800

    Update Api.js
    Signed-off-by: xc-link <cai_liyong@qq.com>

--- Api.js
+++ Api.js
@@ -1,6 +1,13 @@
+//const KeyAuthApp = new KeyAuth(
+  //  "AJX", // Application Name
+ //   "o0dHuOg0Bv", // Owner ID
+   // "4429f44d12c4e16064382e27b498b46db5d5fd0058967a0584ab85511daa70e1", // Application Secret
+    //"1.0", // Application Version
+//);
+
 const KeyAuthApp = new KeyAuth(
     "AJX", // Application Name
     "o0dHuOg0Bv", // Owner ID
-    "4429f44d12c4e16064382e27b498b46db5d5fd0058967a0584ab85511daa70e1", // Application Secret
+    "6d7be1b76efd201da6907bd49d15d37d784f2ff81d8c257c19ed9e89913dac15", // Application Secret
     "1.0", // Application Version
 );
