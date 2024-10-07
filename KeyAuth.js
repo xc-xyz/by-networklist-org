aea76be5f00771546fad36b124f15746410105e8
xc-link <cai_liyong@qq.com>
2024-10-07 11:15:14 +0800

    Create KeyAuth.js
    Signed-off-by: xc-link <cai_liyong@qq.com>

---
+++ KeyAuth.js
@@ -0,0 +1,6 @@
+const KeyAuthApp = new KeyAuth(
+    "AJX", // Application Name
+    "o0dHuOg0Bv", // Owner ID
+    "64665fea2c69a019e85573695a89ba255b72d0157a8d824a8a62868051980eb9", // Application Secret
+    "1.0", // Application Version
+);
