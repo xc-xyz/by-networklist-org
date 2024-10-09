e1569eaa5aed8b9d22c282ba28ba6e954f55274c
xc-link <cai_liyong@qq.com>
2024-10-09 10:15:53 +0800

    Update and rename Api.js to KeyAuth.js
    Signed-off-by: xc-link <cai_liyong@qq.com>

--- KeyAuth.js
+++ KeyAuth.js
@@ -8,6 +8,6 @@
 const KeyAuthApp = new KeyAuth(
     "AJX", // Application Name
     "o0dHuOg0Bv", // Owner ID
-    "64665fea2c69a019e85573695a89ba255b72d0157a8d824a8a62868051980eb9", // Application Secret
+    "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", // Application Secret
     "1.0", // Application Version
 );
