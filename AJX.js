3824331a67931a716e8a7479a6aaf91301c6d0a1
xc-link <cai_liyong@qq.com>
2024-10-29 09:35:55 +0800

    Update AJX.js
    Signed-off-by: xc-link <cai_liyong@qq.com>

--- AJX.js
+++ AJX.js
@@ -3,4 +3,5 @@ const KeyAuthApp = new KeyAuth(
     "o0dHuOg0Bv", // Owner ID
     "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", // Application Secret
     "1.0", // Application Version
+    "d34296d837289aaea0b60b680e173b18", // Token Path (PUT NULL OR LEAVE BLANK IF YOU DO NOT WANT TO USE THE TOKEN VALIDATION SYSTEM! MUST DISABLE VIA APP SETTINGS)
 );
